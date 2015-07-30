57ec57c2998c60040856a172c3ba0dfc30aff798
kendall libby <klibby@mozilla.com>
2015-07-30 09:15:02 -0400

    add hack for rust building; move builds back to ubuntu image

--- docker/ubuntu-indexer/bin/index.sh
+++ docker/ubuntu-indexer/bin/index.sh
@@ -4,4 +4,8 @@
 # Grab the latest config
 wget -q -O dxr.config https://github.com/klibby/dxr-docker/raw/rust/dxr.config
 
+# hack: rust indexing is crazy memory intensive
+if [ 'rust' == "$1" ]; then
+    sed -i -e 's/^workers = ./workers = 2/' dxr.config
+fi
 exec venv/bin/dxr index -c dxr.config -v "$1"
