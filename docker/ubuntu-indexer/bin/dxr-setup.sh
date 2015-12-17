0018aba8716bba5dc5ceab0c8d49070357fb2db9
kendall libby <klibby@mozilla.com>
2015-12-17 16:16:13 -0500

    use clang/llvm-3.5 like dxr does

--- docker/ubuntu-indexer/bin/dxr-setup.sh
+++ docker/ubuntu-indexer/bin/dxr-setup.sh
@@ -32,6 +32,7 @@ fi
 git clone --recursive https://github.com/mozilla/dxr && \
     (cd dxr && git checkout $REV)
 
+#export PATH=/usr/lib/llvm-3.5/bin/:${PATH}
 virtualenv venv
 env VIRTUAL_ENV=`pwd`/venv CC=clang CXX=clang++ make -C dxr
 
