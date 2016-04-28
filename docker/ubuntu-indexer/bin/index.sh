c8cb99bc5852cab5da6b07412b4908747d69a15c
kendall libby <klibby@mozilla.com>
2016-04-28 10:21:07 -0400

    hack: drop rust workers to one to prevent OOM

--- docker/ubuntu-indexer/bin/index.sh
+++ docker/ubuntu-indexer/bin/index.sh
@@ -6,6 +6,6 @@ wget -q -O dxr.config https://github.com/klibby/dxr-docker/raw/master/dxr.config
 
 # hack: rust indexing is crazy memory intensive
 if [ 'rust' == "$1" ]; then
-    sed -i -e 's/^workers = ./workers = 2/' dxr.config
+    sed -i -e 's/^workers = ./workers = 1/' dxr.config
 fi
 exec venv/bin/dxr index -c dxr.config -v "$1"
