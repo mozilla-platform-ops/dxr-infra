a36d2e994ea3fd1a5f68f1b2d77224a78d9ae199
kendall libby <klibby@mozilla.com>
2016-06-21 11:10:00 -0400

    ansible: rsync addons data from cloudops

---
+++ ansible/roles/admin/files/rsync-addons.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+LOCKFILE="/tmp/rsync-addons-dxr.lock"
+AMOADMIN="opsadmin.amo.us-west-2.prod.mozaws.net"
+LOGFILE="$HOME/log/rsync-dxr.log"
+
+(
+  flock -x -w 10 200 || exit 1
+  rsync -avz -e ssh --delete -f '- temp/' --delete-before $AMOADMIN:/data/addons/ /data/dxr_data/addons 2>&1 >> $LOGFILE
+) 200>$LOCKFILE
