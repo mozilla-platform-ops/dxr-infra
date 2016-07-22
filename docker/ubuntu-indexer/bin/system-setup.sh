1b6da6148ea0ea9e32649ff1f5ba77b9b3d665a9
kendall libby <klibby@mozilla.com>
2016-07-22 14:05:03 -0400

    docker: revert back to ubuntu 14.04 LTS and clang-3.6
    16.04+clang run into an issue with mixed ABIs, between clang and DXR's
    clang plugin. The TROUBLESHOOTING section at
    https://github.com/nyalldawson/clazy describes the problem, with
    relevant links. Should be fixed in subsequent builds of clang on Xenial.

--- docker/ubuntu-indexer/bin/system-setup.sh
+++ docker/ubuntu-indexer/bin/system-setup.sh
@@ -20,12 +20,13 @@ apt_packages+=('git')
 apt_packages+=('jq')
 apt_packages+=('gawk')
 apt_packages+=('sudo')
+apt_packages+=('vim')
 
 # Dependencies for DXR
-apt_packages+=('clang-3.7')
-apt_packages+=('libclang-3.7-dev')
-apt_packages+=('llvm-3.7')
-apt_packages+=('llvm-3.7-dev')
+apt_packages+=('clang-3.6')
+apt_packages+=('libclang-3.6-dev')
+apt_packages+=('llvm-3.6')
+apt_packages+=('llvm-3.6-dev')
 apt_packages+=('npm')
 
 # Dependency for nodejs
@@ -49,7 +50,7 @@ apt_packages+=('libgstreamer-plugins-base0.10-dev')
 apt-get update -y
 
 apt-get install -y --force-yes ${apt_packages[@]}
-apt-get build-dep -y clang-3.7 llvm-3.7
+apt-get build-dep -y clang-3.6 llvm-3.6
 
 # Add a newer version of node that supports ES6.
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
